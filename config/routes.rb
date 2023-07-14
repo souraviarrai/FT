2eff48caa243531b82f635515437b5c805e8cc09
Sourav Rai <souraviarrai@gmail.com>
2023-07-14 17:55:08 +0600

    view to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/my_portfolio'
   root "welcome#index"
   devise_for :users
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
 end
